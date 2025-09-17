4525097c4695c4207403de75a76e1ceefd43de45
Cindy GY <162321776+Wu-Cindy-GuanYing@users.noreply.github.com>
2025-09-17 20:29:37 +0200

    Third Commit- Adjust _Layout

--- ContractMonthlyClaimSystem/obj/Debug/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
+++ ContractMonthlyClaimSystem/obj/Debug/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c054bcf6c068d1d7b98600a017ef479d26b4bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5989bfb0be79282dda25b0e2d1e94c55b2fcf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
