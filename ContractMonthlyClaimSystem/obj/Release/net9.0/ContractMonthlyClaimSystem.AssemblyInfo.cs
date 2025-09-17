cf5989bfb0be79282dda25b0e2d1e94c55b2fcf0
Cindy GY <162321776+Wu-Cindy-GuanYing@users.noreply.github.com>
2025-09-17 20:26:34 +0200

    Second commit

--- ContractMonthlyClaimSystem/obj/Release/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
+++ ContractMonthlyClaimSystem/obj/Release/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContractMonthlyClaimSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c054bcf6c068d1d7b98600a017ef479d26b4bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContractMonthlyClaimSystem")]
